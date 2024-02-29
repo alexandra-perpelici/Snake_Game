ae6433db31a8a8e7b27e11e668009ccab59718a5
Alexandra <126694896+alexandra-perpelici@users.noreply.github.com>
2024-02-29 16:54:01 +0200

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    GameFrame()
+    {
+//       GamePanel panel = new GamePanel();
+//       this.add(panel); OR:
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null); // appear in the middle of the screen
+
+
+
+    }
+}
